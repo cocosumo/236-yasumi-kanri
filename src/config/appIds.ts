a018a870154e71e9f097fe4dc9e57181759bfc3d
Lorenz Ras <lenzras@gmail.com>
2024-01-08 17:07:52 +0900

    add appId

--- src/config/appIds.ts
+++ src/config/appIds.ts
@@ -1,2 +1,8 @@
+import {isProd} from '@helpers/isProd';
+
 export const prodAppId = 108;
 export const devAppId = 130;
+
+export const appId = isProd ? prodAppId : devAppId;
+
+console.log('appId', appId);
