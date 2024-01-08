abae19972233679216c9fb3859f0e637659d18e5
Lorenz Ras <lenzras@gmail.com>
2024-01-08 17:08:18 +0900

    Update appIds.ts

--- src/config/appIds.ts
+++ src/config/appIds.ts
@@ -3,6 +3,7 @@ import {isProd} from '@helpers/isProd';
 export const prodAppId = 108;
 export const devAppId = 130;
 
+/** 休み管理のアプリ番号 */
 export const appId = isProd ? prodAppId : devAppId;
 
 console.log('appId', appId);
